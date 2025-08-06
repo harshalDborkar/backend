940cf59b237672e8e25ed669d3b04812dc119d61
harshal borkar <harshal.deepak.borkar@gmail.com>
2025-08-06 23:01:23 +0530

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("Aap twitter pe hai janab")
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<button>hwllo ji </button>')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
